3f9004d9c7cea2f055072f9921a1c074aa9a14ad
Shivamkharade <shivamkharade189@gmail.com>
2026-04-01 17:21:39 +0530

    ProductApis Complete and checked

--- src/main/java/com/e_comm/Controller/ProductController.java
+++ src/main/java/com/e_comm/Controller/ProductController.java
@@ -3,7 +3,10 @@ package com.e_comm.Controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.catalina.mapper.Mapper;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -12,14 +15,22 @@ import com.e_comm.service.ProductService;
 import com.petstore.api.ProductsApi;
 import com.petstore.model.Product;
 
+import ch.qos.logback.core.model.Model;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
 @RestController
 public class ProductController implements ProductsApi{
 
-	@Autowired
-	private ProductService productService;
+	
+	private final ProductService productService;
+	
+	private final ModelMapper mapper;
+	
+	public ProductController(ProductService productService1) {
+		this.productService = productService1;
+		this.mapper = new ModelMapper();
+	}
 
 	@Override
 	public Optional<NativeWebRequest> getRequest() {
@@ -29,29 +40,28 @@ public class ProductController implements ProductsApi{
 
 	@Override
 	public ResponseEntity<List<Product>> productsGet() {
-		// TODO Auto-generated method stub
-		return ProductsApi.super.productsGet();
-	}
-
-	@Override
-	public ResponseEntity<Void> productsIdDelete(@NotNull String id) {
-		// TODO Auto-generated method stub
-		return ProductsApi.super.productsIdDelete(id);
+		List<Product> list = productService.productsGet().stream()
+														  .map(entity -> mapper.map(entity, Product.class))
+														  .toList();
+		return ResponseEntity.ok(list);
 	}
 
 	@Override
 	public ResponseEntity<Product> productsIdGet(@NotNull String id) {
-		// TODO Auto-generated method stub
-		return ProductsApi.super.productsIdGet(id);
+		Product product = mapper.map(productService.productsIdGet(id), Product.class);
+		return ResponseEntity.ok(product);
+	}
+	
+	@Override
+	public ResponseEntity<Void> productsIdDelete(@NotNull String id) {
+		productService.productsIdDelete(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 
 	@Override
-	public ResponseEntity<Void> productsPost(@Valid Product product) {
-		// TODO Auto-generated method stub
-		return ProductsApi.super.productsPost(product);
-	}
-
-	
-	
+	public ResponseEntity<Product> productsPost(@Valid Product product) {
+		Product product2 = mapper.map(productService.productsPost(product), Product.class);
+		return new ResponseEntity<>(product2,HttpStatus.CREATED);
+	}	
 	
 }
