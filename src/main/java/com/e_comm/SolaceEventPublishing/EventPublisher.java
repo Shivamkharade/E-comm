fb5397177311937e97fccae20bea747531de86a7
Shivamkharade <shivamkharade189@gmail.com>
2026-04-22 16:30:51 +0530

    added event publisher

---
+++ src/main/java/com/e_comm/SolaceEventPublishing/EventPublisher.java
@@ -0,0 +1,20 @@
+package com.e_comm.SolaceEventPublishing;
+
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class EventPublisher {
+
+	private final StreamBridge streamBridge;
+	
+	public EventPublisher(StreamBridge streamBridge1) {
+		this.streamBridge = streamBridge1;
+	}
+	
+	public void publish(EventSent event) {
+		streamBridge.send("userJwtLogin-out-0", event);
+	}
+	
+}
